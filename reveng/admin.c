f3198325099381f60eb8466152375e2ab0e9810e
Ryan Burrow <rsardb11@vt.edu>
2018-03-16 12:38:26 -0400

    added makefile and reveng2.c

--- reveng/admin.c
+++ reveng/admin.c
@@ -2,12 +2,17 @@
 #include <string.h>
 #include "reveng.h"
 
-static unsigned char FLAG0[FLAGLEN] = "revengflag000";
-static unsigned char FLAG1[FLAGLEN] = "revengflag000";
-static unsigned char FLAG2[FLAGLEN] = "revengflag000";
+static unsigned char FLAG0[FLAGLEN] = "revengflag00000";
+static unsigned char FLAG1[FLAGLEN] = "revengflag00001";
+static unsigned char FLAG2[FLAGLEN] = "revengflag00002";
 
-int main(int argc, char **argv) {
+int main() {
+	char tmp[16];
 	// generate the flag
-	genFlag0(FLAG0);
-	printf("flag{%s}\n", FLAG0);
+	memset(tmp, '\0', 16);
+	strncpy(tmp, FLAG0, 15);
+	
+	genFlag0(tmp);
+	printf("Flag0: flag{%s}\n", FLAG0);
+	printf("Value to write to reveng0.c: %s\n", tmp);
 }
--- reveng/flaggen.c
+++ reveng/flaggen.c
@@ -6,13 +6,15 @@ void genFlag0(char *flag) {
 	srand(PI);
 	int max = rand() % PI;
 	for (int i = 0; i < max; i++) {
-		int spot = rand() % FLAGLEN;
+		int spot = rand() % (FLAGLEN - 1);
 		do {
 			uint8_t xor = rand() % 0x7F;
 			flag[spot] ^= xor;
 		} while ((flag[spot] < RANGEMIN) || (flag[spot] > RANGEMAX));
 	}
 }
+
+
 
 char makePrintable(unsigned char c) {
 	return (c % (RANGEMAX - RANGEMIN)) + RANGEMIN;
