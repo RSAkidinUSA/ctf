b7919cdf54bb756db8843edca3a0fa373280fe75
Ryan Burrow <rsardb11@vt.edu>
2018-03-16 15:06:46 -0400

    Program now dynamically makes flags.h

--- reveng/admin.c
+++ reveng/admin.c
@@ -4,11 +4,44 @@
 
 static unsigned char FLAG0[FLAGLEN] = "xxRevEngFlag0xx";
 static unsigned char FLAG1[FLAGLEN] = "b1t0ps4reFUN001";
-static unsigned char FLAG1[FLAGLEN] = "b1t0ps4reFUN001";
+static unsigned char FLAG2[FLAGLEN] = "CheckTheRetVals";
 static unsigned char FLAG3[FLAGLEN] = "r3v3ng13stfl4g3";
+
+// check if character needs to be escapable
+int escapable(char c) {
+	switch (c) {
+		case '\'':
+		case '%':
+		case '"':
+		case '\\':
+		case '\?':
+			return 1;
+		default:
+			return 0;
+	}
+}
+
+// write flags to header file
+void writeHeader(int flagNum, char *flag, FILE *f) {
+	char tmp[64], tmpFlag[64];
+	memset(tmp, 0, 64);
+	memset(tmpFlag, 0, 64);
+	int count = 0;
+	for (int i = 0; i < FLAGLEN; i++) {
+		// check valid chars
+		if (escapable(flag[i])) {
+			tmpFlag[count++] = '\\';
+		}
+		tmpFlag[count++] = flag[i];
+	}
+	sprintf(tmp, "unsigned char FLAG%d[FLAGLEN] = \"%s\";\n", flagNum, tmpFlag);
+	fputs(tmp, f);
+}
 
 int main() {
 	char tmp[16];
+	FILE *f = fopen("flags.h", "w");
+	fputs("#include \"reveng.h\"\n\n", f);
 	// generate the flag for attack 0
 	memset(tmp, '\0', 16);
 	strncpy(tmp, FLAG0, 15);
@@ -16,6 +49,7 @@ int main() {
 	genFlag0(tmp);
 	printf("Flag0: flag{%s}, ", FLAG0);
 	printf("Value: %s\n", tmp);
+	writeHeader(0, tmp, f);
 
 	// generate the flag for attack 1
 	memset(tmp, '\0', 16);
@@ -24,6 +58,7 @@ int main() {
 	genFlag1(tmp);
 	printf("Flag1: flag{%s}, ", FLAG1);
 	printf("Value: %s\n", tmp);
+	writeHeader(1, tmp, f);
 
 	// generate the flag for attack 2
 	memset(tmp, '\0', 16);
@@ -32,14 +67,18 @@ int main() {
 	genFlag2(tmp);
 	printf("Flag2: flag{%s}, ", FLAG2);
 	printf("Value: %s\n", tmp);
+	writeHeader(2, tmp, f);
 
 	// generate the flag for attack 2
 	memset(tmp, '\0', 16);
 	strncpy(tmp, FLAG3, 15);
 	
-	genFlag2(tmp);
+	genFlag3(tmp);
 	printf("Flag3: flag{%s}, ", FLAG3);
 	printf("Value: %s\n", tmp);
+	writeHeader(3, tmp, f);
 
+	fflush(f);
+	fclose(f);
 	return 0;
 }
--- reveng/flags.h
+++
@@ -1,6 +0,0 @@
-#include "reveng.h"
-
-unsigned char FLAG0[FLAGLEN] = "[5HY(QJ)ODJp[[[";
-unsigned char FLAG1[FLAGLEN] = "R&eoJf1!EFr\%@\"B";
-unsigned char FLAG2[FLAGLEN] = "1eeoSFa!4fq\%NVB";
-unsigned char FLAG3[FLAGLEN] = "IO<d]bCT:gKawIK";
