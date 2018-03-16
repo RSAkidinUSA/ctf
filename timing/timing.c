eadc39f8a218cfedbad73927acbe97e6869fbd8a
Ryan Burrow <rsardb11@vt.edu>
2018-03-16 11:07:20 -0400

    Updated timing attack

--- timing/timing.c
+++ timing/timing.c
@@ -2,45 +2,57 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <time.h>
 
 #define FLAGLEN 16
+#define PASSLEN 16
+
+static unsigned char FLAG[FLAGLEN] = "t1m1nGfjau39aFD";
+static unsigned char PASS[PASSLEN] = "PASSWORD1234567";
+
 #define RANGEMIN 40
 #define RANGEMAX 126
-#define SEED 562951413
 
-static unsigned char FLAG[FLAGLEN] = "revengflag000";
-
-void genFlag(void) {
-	srand(SEED);
-	int max = rand() % (SEED / 2);
+void genPass(void) {
+	int seed = time(NULL);
+	srand(seed);
+	int max = rand() % 500;
 	for (int i = 0; i < max; i++) {
-		int spot = rand() % FLAGLEN;
+		int spot = rand() % PASSLEN;
 		do {
 			uint8_t xor = rand() % 0x7F;
-			FLAG[spot] ^= xor;
-		} while ((FLAG[spot] < RANGEMIN) || (FLAG[spot] > RANGEMAX));
+			PASS[spot] ^= xor;
+		} while ((PASS[spot] < RANGEMIN) || (PASS[spot] > RANGEMAX));
 	}
 }
 
-int main(int argc, char **argv) {
-	if (argc != 2 || (strlen(argv[1]) == 0)) {
-		printf("Usage: %s guess\n", argv[0]);
-		return(1);
-	}
-	char tmp[51];
-	memset(tmp, 0, 51);
-	// generate the flag
-	genFlag();
-	sprintf(tmp, "flag{%s}", FLAG);
-	// Uncomment the following line only to determine the flag to save
-	// printf("flag{%s}\n", FLAG);
-
-	for (int i = 0; i < FLAGLEN + strlen("flag{") + strlen("}"); i++) {
-		if (tmp[i] != argv[1][i]) {
-			printf("Guess, %s, is not correct!\n", argv[1]);
-			return 1;
+int checkflag (char *guess) {
+	// null terminate password
+	guess[15] = '\0';
+	// check password
+	for (int i = 0; i < PASSLEN; i++) {
+		if (PASS[i] != guess[i]) {
+			printf("Incorrect password, try again:\n");
+			fflush(stdout);
+			return 0;
 		}
 	}
-	printf("Guess, %s, was correct!\n", argv[1]);
-	return 0;
+	printf("Correct password!\n");
+	fflush(stdout);
+	return 1;
+}
+
+int main(int argc, char **argv) {
+	genPass();
+	char guess[64];
+
+	printf("Please enter the password to get the stopwatch flag:\n");
+	fflush(stdout);
+	for (;;) {
+		memset(guess, 0, 64);
+		if (checkflag(fgets(guess, 64, stdin))) {
+			break;
+		} 
+	}
+	return 0;	
 }
